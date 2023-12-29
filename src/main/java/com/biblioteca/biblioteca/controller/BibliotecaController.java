96d916d9b53fa9abe0cc52ebaf0e4bb7b3de320e
rodrigogama <rodrigogamah4@gmail.com>
2023-12-29 10:25:18 -0300

    post

--- src/main/java/com/biblioteca/biblioteca/controller/BibliotecaController.java
+++ src/main/java/com/biblioteca/biblioteca/controller/BibliotecaController.java
@@ -11,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,10 @@ import com.biblioteca.biblioteca.domain.Autor;
 import com.biblioteca.biblioteca.domain.Emprestimo;
 import com.biblioteca.biblioteca.domain.Livros;
 import com.biblioteca.biblioteca.domain.Usuario;
+import com.biblioteca.biblioteca.dtos.AutorDTO;
+import com.biblioteca.biblioteca.dtos.EmprestimoDTO;
+import com.biblioteca.biblioteca.dtos.LivrosDTO;
+import com.biblioteca.biblioteca.dtos.UsuarioDTO;
 import com.biblioteca.biblioteca.services.AutorService;
 import com.biblioteca.biblioteca.services.EmprestimoService;
 import com.biblioteca.biblioteca.services.LivrosService;
@@ -53,6 +59,11 @@ public class BibliotecaController {
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @PostMapping("/usuarios")
+    public void saveUser(@RequestBody UsuarioDTO usuarioDTO ){
+        Usuario user = new Usuario(usuarioDTO);
+        this.usuarioService.saveUser(user);
+    }
     
 
     /*CRUD de autores */
@@ -69,6 +80,12 @@ public class BibliotecaController {
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @PostMapping("autores")
+    public void saveAuth(@RequestBody AutorDTO autorDTO){
+        Autor autor = new Autor(autorDTO);
+        this.autorService.saveAuth(autor);
+    }
+
     /*CRUD de livros */
     @GetMapping("/livros")
     public ResponseEntity<Page<Livros>> getAllBooks(Pageable pageable){
@@ -83,6 +100,12 @@ public class BibliotecaController {
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @PostMapping("livros")
+    public void saveBook(@RequestBody LivrosDTO livrosDTO){
+        Livros livro = new Livros(livrosDTO);
+        this.livrosService.saveBook(livro);
+    }
+
     /*CRUD de usuários */
     @GetMapping("/emprestimos")
     public ResponseEntity<Page<Emprestimo>> getAllLoan(Pageable pageable){
@@ -90,10 +113,16 @@ public class BibliotecaController {
         return new ResponseEntity<Page<Emprestimo>>(emprestimos, HttpStatus.OK);
     }
 
-    @GetMapping("/livros/{id}")
+    @GetMapping("/emprestimos/{id}")
     public ResponseEntity<Emprestimo> getLoanById(@PathVariable UUID id){
         Optional<Emprestimo> emprestimo = this.emprestimoService.findbyId(id);
         return emprestimo.map(e -> new ResponseEntity<>(e, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("emprestimos")
+    public void saveLoad(@RequestBody EmprestimoDTO emprestimoDTO){
+        Emprestimo emprestimo = new Emprestimo(emprestimoDTO);
+        this.emprestimoService.saveLoad(emprestimo);
     }
 }
