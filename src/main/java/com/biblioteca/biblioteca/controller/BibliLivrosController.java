5a45d68ccd94088b486fde102466ffd36c05ddf1
rodrigogama <rodrigogamah4@gmail.com>
2023-12-30 14:27:11 -0300

    mapeando

--- src/main/java/com/biblioteca/biblioteca/controller/BibliLivrosController.java
+++ src/main/java/com/biblioteca/biblioteca/controller/BibliLivrosController.java
@@ -7,18 +7,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.biblioteca.biblioteca.domain.Livros;
 import com.biblioteca.biblioteca.dtos.LivrosDTO;
 import com.biblioteca.biblioteca.services.LivrosService;
 
-@Service
+@RestController
+@RequestMapping("biblioteca")
 public class BibliLivrosController {
 
     @Autowired
--- src/main/java/com/biblioteca/biblioteca/controller/BibliUsuariosController.java
+++ src/main/java/com/biblioteca/biblioteca/controller/BibliUsuariosController.java
@@ -8,21 +8,23 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.biblioteca.biblioteca.domain.Usuario;
 import com.biblioteca.biblioteca.dtos.UsuarioDTO;
 import com.biblioteca.biblioteca.services.UsuarioService;
 
-@Service
+@RestController
+@RequestMapping("biblioteca")
 public class BibliUsuariosController {
-        @Autowired
-        private UsuarioService usuarioService;
+    @Autowired
+    private UsuarioService usuarioService;
 
 
     @GetMapping("/usuarios")
