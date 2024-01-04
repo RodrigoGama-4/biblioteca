730d6e914cd858ffeb9cc726595d6bd8357c3d54
rodrigogama <rodrigogamah4@gmail.com>
2024-01-04 10:40:13 -0300

    anotações diferentes

--- src/main/java/com/biblioteca/biblioteca/Exceptio/LivroNaoEncontrado.java
+++
@@ -1,7 +0,0 @@
-package com.biblioteca.biblioteca.Exceptio;
-
-public class LivroNaoEncontrado extends RuntimeException{
-    public LivroNaoEncontrado(String isbn) {
-        super("Livro não encontrado para o ISBN: " + isbn);
-    }
-}
--- src/main/java/com/biblioteca/biblioteca/Exceptio/LivrosOcupados.java
+++
@@ -1,7 +0,0 @@
-package com.biblioteca.biblioteca.Exceptio;
-
-public class LivrosOcupados extends RuntimeException {
-    public LivrosOcupados(String message) {
-        super(message);
-    }
-}
