29e00f8f771e764b82a6c0d8b92937433ba6c1c0
rodrigogama <rodrigogamah4@gmail.com>
2024-01-06 21:33:32 -0300

    filtro de segurança

---
+++ src/main/java/com/biblioteca/biblioteca/dtos/LoginResponseDTO.java
@@ -0,0 +1,5 @@
+package com.biblioteca.biblioteca.dtos;
+
+public record LoginResponseDTO(String token) {
+    
+}
---
+++ src/main/java/com/biblioteca/biblioteca/infra/security/SecurityFilter.java
@@ -0,0 +1,45 @@
+package com.biblioteca.biblioteca.infra.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.biblioteca.biblioteca.services.UsuarioService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if(token != null){
+            var login = tokenService.validateToken(token);
+            UserDetails user = this.usuarioService.findByLogin(login);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
