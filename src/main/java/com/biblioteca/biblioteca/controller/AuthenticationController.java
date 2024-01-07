1cfe6ebf9d9362a83ef053aed4d4f09f6b2496bf
rodrigogama <rodrigogamah4@gmail.com>
2024-01-06 21:33:01 -0300

    login authentication

--- src/main/java/com/biblioteca/biblioteca/controller/AuthenticationController.java
+++ src/main/java/com/biblioteca/biblioteca/controller/AuthenticationController.java
@@ -8,11 +8,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.biblioteca.biblioteca.domain.Usuario;
+import com.biblioteca.biblioteca.dtos.AuthenticationDTO;
+import com.biblioteca.biblioteca.dtos.LoginResponseDTO;
 import com.biblioteca.biblioteca.dtos.UsuarioDTO;
+import com.biblioteca.biblioteca.infra.security.TokenService;
 import com.biblioteca.biblioteca.services.UsuarioService;
 
 import jakarta.validation.Valid;
@@ -24,6 +28,12 @@ public class AuthenticationController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping("/register")
     public ResponseEntity<Usuario> register(@RequestBody @Valid UsuarioDTO data){
         if(this.usuarioService.findbyEmail(data.email()) != null) return ResponseEntity.badRequest().build();
@@ -34,6 +44,16 @@ public class AuthenticationController {
         this.usuarioService.saveUser(user);
     
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid AuthenticationDTO data ){
+        var userNameSenha = new UsernamePasswordAuthenticationToken(data.email(), data.senha());
+        var auth = this.authenticationManager.authenticate(userNameSenha);
+
+        var token = tokenService.generateToken((Usuario)auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
     }
  
 }
