755a18c85ec864b9bab000a591c9fc1e62b53f61
rodrigogama <rodrigogamah4@gmail.com>
2024-01-08 17:20:01 -0300

    mandar email aos devedores meio dia de todos os dias

--- src/main/java/com/biblioteca/biblioteca/repository/EmprestimoRepository.java
+++ src/main/java/com/biblioteca/biblioteca/repository/EmprestimoRepository.java
@@ -1,5 +1,8 @@
 package com.biblioteca.biblioteca.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,5 +12,7 @@ import com.biblioteca.biblioteca.domain.Emprestimo;
 public interface EmprestimoRepository extends JpaRepository<Emprestimo, Long>{
     @Query("SELECT e FROM Emprestimo e WHERE e.livro.isbn = :isbn AND e.usuario.userId = :usuarioId")
     Emprestimo findByIsbnAndUsuarioId(@Param("isbn") String isbn, @Param("usuarioId") Long usuarioId);
+
+    List<Emprestimo> findByDataDevolucaoBeforeAndDataDevolucaoIsNotNull(LocalDate dataDevolucao);
 
 }
--- src/main/java/com/biblioteca/biblioteca/services/mail/MailService.java
+++ src/main/java/com/biblioteca/biblioteca/services/mail/MailService.java
@@ -1,11 +1,20 @@
 package com.biblioteca.biblioteca.services.mail;
 
+import java.time.LocalDate;
+import java.util.List;
+
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import com.biblioteca.biblioteca.domain.Emprestimo;
+import com.biblioteca.biblioteca.repository.EmprestimoRepository;
+import com.biblioteca.biblioteca.services.EmprestimoService;
 
 import org.springframework.beans.factory.annotation.Value;
 
@@ -20,8 +29,23 @@ public class MailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Autowired
+    private EmprestimoRepository emprestimoRepository;
+
     @Value("${spring.mail.username}")
     private String fromMail;
+
+
+
+    @Scheduled(cron = "0 0 12 * * ?")
+    public void senderMailBilling(){
+        List<Emprestimo> emprestimos = this.buscarUsuariosComDevolucaoAtrasada();
+
+        for (Emprestimo emprestimo: emprestimos){
+            this.senderMail(emprestimo.getUsuario().getNome(), emprestimo.getUsuario().getEmail(),
+             "DIA DA DEVOLUÇÃO", "Prezado, o dia da devolução do livro chegou, por favor nos entregue o livro: " + emprestimo.getLivro().getTitulo() + "que estar pendente.");
+        }
+    }
 
     public void senderMail(String nome, String email, String tituloEmail, String corpoEmail){
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
@@ -33,6 +57,12 @@ public class MailService {
 
         mailSender.send(simpleMailMessage);
         LOGGER.info("Email enviado com sucesso ao endereço: " + email);
+    }
+
+    /*Retorna a lista de emprestimos que estao para devolução */
+    public List<Emprestimo> buscarUsuariosComDevolucaoAtrasada() {
+        LocalDate dataAtual = LocalDate.now();
+        return emprestimoRepository.findByDataDevolucaoBeforeAndDataDevolucaoIsNotNull(dataAtual);
     }
 
 }
